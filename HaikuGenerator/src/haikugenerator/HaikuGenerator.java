4536ee84f6637f6f540afcacef97c9057a3c49d0
MaxMagnuson <magnu401@morris.umn.edu>
2016-12-09 13:27:40 -0600

    Added more methods to RandomWalk generator for shortest steps, median steps, and most steps.

--- HaikuGenerator/src/haikugenerator/HaikuGenerator.java
+++ HaikuGenerator/src/haikugenerator/HaikuGenerator.java
@@ -6,6 +6,7 @@
 package haikugenerator;
 
 import haikugenerator.Interface.IGenerator;
+import java.util.ArrayList;
 
 /**
  *
@@ -16,22 +17,33 @@ public class HaikuGenerator {
     /**
      * @param args the command line arguments
      */
-    //private static final String path = "C:\\Users\\MaxM\\Documents\\GitHub\\HaikuGenerator\\AesopLyrics.txt";
-    private static final String path = "D:\\Documents\\Schoolwork\\AI\\HaikuGenerator\\AesopLyrics.txt";
+    private static final String path = "C:\\Users\\MaxM\\Documents\\GitHub\\HaikuGenerator\\AesopLyrics.txt";
+    private static final String pathTwo = "C:\\Users\\MaxM\\Documents\\GitHub\\HaikuGenerator\\ShakespeareUnabridged.txt";
+    //private static final String path = "D:\\Documents\\Schoolwork\\AI\\HaikuGenerator\\AesopLyrics.txt";
     public static void main(String[] args) {
         // TODO code application logic here
         // Note: Feel Free to edit this with whatever test stuff
         
+        ArrayList<String> files = new ArrayList<String>();
+        files.add(path);
+        files.add(pathTwo);
+        
         Parser p = new Parser();
-        MarkovChain mc = p.ParseFile(path, true);
-        mc.PrintChain();
+        MarkovChain mc = p.ParseFile(pathTwo, true);
+        //mc.PrintChain();
         
         IGenerator gen = new NaiveGenerator(mc);
-        for(int i = 0; i < 0; i++)
-        {
-            System.out.println("Running iteration " + i);
-            System.out.println(gen.GenerateHaiku().toString());
-        }
+        ShortRandomWalk_Generator collGen = new ShortRandomWalk_Generator(mc);
+        //for(int i = 0; i < 0; i++)
+        //{
+            //System.out.println("Running iteration " + i);
+            ArrayList<Haiku> haikus = collGen.GenHaikuCollection();
+            for(int j = 0; j < haikus.size(); j++)
+            {
+                System.out.println(haikus.get(j).toString());
+            }
+            //System.out.println(gen.GenerateHaiku().toString());
+        //}
     }
     
 }
--- HaikuGenerator/src/haikugenerator/ShortRandomWalk_Generator.java
+++ HaikuGenerator/src/haikugenerator/ShortRandomWalk_Generator.java
@@ -9,6 +9,7 @@ import haikugenerator.Haikus.FiveLine;
 import haikugenerator.Haikus.IHaikuLine;
 import haikugenerator.Haikus.SevenLine;
 import haikugenerator.Interface.IGenerator;
+import java.util.ArrayList;
 
 /**
  *
@@ -64,6 +65,39 @@ public class ShortRandomWalk_Generator implements IGenerator {
         return new Haiku(firstLine.GetLine(), secondLine.GetLine(), thirdLine.GetLine());
     }
     
+    public ArrayList<Haiku> GenHaikuCollection()
+    {
+        MarkovNode root = this.chain.GetRandomNode();
+        
+        ArrayList<Walk> fives = new ArrayList<Walk>();
+        for(int i = 0; i < ITERATIONS; i++)
+        {
+            Walk nextWalk = GenWalk(root, new FiveLine());
+            // Maintains a sorted ascending order by steps
+            fives = InsertWalk(fives, nextWalk);
+        }
+        
+        ArrayList<Walk> sevens = new ArrayList<Walk>();
+        for(int i = 0; i < ITERATIONS; i++)
+        {
+            Walk nextWalk = GenWalk(root, new SevenLine());
+            // Maintains a sorted ascending order by steps
+            sevens = InsertWalk(sevens, nextWalk);
+        }
+        System.out.println("fives size: " + fives.size());
+        System.out.println("sevens size: " + sevens.size());
+        Haiku fewSteps = new Haiku(fives.get(0).GetLine(), sevens.get(0).GetLine(), fives.get(1).GetLine());
+        Haiku midSteps = new Haiku(fives.get(fives.size()/2).GetLine(), sevens.get(sevens.size()/2).GetLine(), fives.get(fives.size()/2 + 1).GetLine());
+        Haiku mostSteps = new Haiku(fives.get(fives.size() - 2).GetLine(), sevens.get(sevens.size()-1).GetLine(), fives.get(fives.size() - 1).GetLine());
+        
+        ArrayList<Haiku> result = new ArrayList<Haiku>();
+        result.add(fewSteps);
+        result.add(midSteps);
+        result.add(mostSteps);
+        
+        return result;
+    }
+    
     private Walk GenWalk(MarkovNode root, IHaikuLine line)
     {
         MarkovNode tempRoot = root;
@@ -81,6 +115,21 @@ public class ShortRandomWalk_Generator implements IGenerator {
             currentWord = currentNode.NextWord();
         }
         return new Walk(line, steps);
+    }
+    
+    private ArrayList<Walk> InsertWalk(ArrayList<Walk> walks, Walk walk)
+    {
+        for(int i = 0; i < walks.size(); i++)
+        {
+            if(walk.GetSteps() < walks.get(i).GetSteps())
+            {
+                walks.add(i, walk);
+                return walks;
+            }
+        }
+        //Runs if walks size is zero
+        walks.add(walk);
+        return walks;
     }
     
     private void SwapWalks(Walk one, Walk two)
